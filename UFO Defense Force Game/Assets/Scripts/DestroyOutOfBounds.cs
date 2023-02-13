ec9cd73ed33824554d104480c76f021504842fc3
Lily Wilson <lilywlson@aol.com>
2023-02-12 23:59:33 -0700

    Challenge Upload
    Challenge 6a

--- UFO Defense Force Game/Assets/Scripts/DestroyOutOfBounds.cs
+++ UFO Defense Force Game/Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,7 +22,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 
         else if(transform.position.z < lowerBounds)
         {
-            Debug.Log("Game Over!");
+           // Debug.Log("Game Over!");
             Destroy(gameObject);
            // Time.timeScale = 0;
         }
