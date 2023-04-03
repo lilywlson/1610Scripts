516e3f81131acc3125aa22e45e87d571ec59646d
Lily Wilson <lilywlson@aol.com>
2023-04-03 16:56:40 -0600

    2D Platformer - Health and Attack Labs

---
+++ 2D Platformer/Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int dmgAmount)
+    {
+        currentHealth -= dmgAmount;
+        Debug.Log("Player Health = "+ currentHealth);
+
+        if(currentHealth <=0)
+        {
+            Debug.Log("GAME OVER!");
+            Time.timeScale = 0;
+        }
+    }
+
+    public void AddHealth(int healAmount)
+    {
+        currentHealth += healAmount;
+
+        if(currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
